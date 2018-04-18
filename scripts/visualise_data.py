e18a710f2c4b4056d3f45bdcba7fb7f49f9776d3
cacad101 <gbbpentium@gmail.com>
2018-04-18 12:22:22 +0800

    add data visualisation and settings file

---
+++ scripts/visualise_data.py
@@ -0,0 +1,37 @@
+""" Script to visualise data from csv """
+
+import os
+import logging
+
+import numpy as np
+import pandas as pd
+
+from utils_highway_call_simulator.utility import init_logger, load_settings, get_settings_path_from_arg
+from utils_highway_call_simulator.visualisation import visualize_histogram
+
+
+def main():
+    file_name = os.path.basename(__file__)[:-3]
+    settings_path = get_settings_path_from_arg(file_name)
+    settings = load_settings(settings_path)
+
+    init_logger(settings.log.path, file_name, settings.log.level)
+    logging.info("[{}] Logging initiated".format(file_name))
+
+    logging.info("[{}] Input data loaded from {}".format(file_name, settings.data.real_input))
+    real_input = pd.read_csv(settings.data.real_input)
+
+    save_to = settings.data.image_file
+    plot = False
+    bins = 20
+    for col in real_input.columns:
+        logging.info("[{}] Visualizing data data:{}, save_to:{}, plot:{}".format(
+            file_name, col, save_to, plot
+            ))
+        visualize_histogram(real_input[col], "_".join(col.split()[:-1]), save_to=save_to, plot=plot, bins=bins)
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils_highway_call_simulator/visualisation.py
@@ -0,0 +1,22 @@
+""" Data visualisation Utility """
+
+import os
+
+import matplotlib.pyplot as plt
+
+from utils_highway_call_simulator.utility import ensure_dir, get_now_str
+
+
+def visualize_histogram(data, title, save_to="", plot=False, bins='auto'):
+    """ Visualise data as histogram, optional 'plot' and 'save_to' args """
+    plt.hist(data, bins=bins)
+    plt.title(title)
+    plt.xlabel("Value")
+    plt.ylabel("Frequency")
+    if save_to:
+        ensure_dir(save_to)
+        file_name = os.path.join(save_to, get_now_str()+"_"+title+".png")
+        plt.savefig(file_name)
+    if plot:
+        plt.show()
+    plt.close()
