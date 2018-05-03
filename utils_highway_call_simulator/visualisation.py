4b2305e785da79b7ce7a8d1c8e3a8eaf7d458164
cacad101 <gbbpentium@gmail.com>
2018-05-03 15:56:37 +0800

    add line visualizer

--- utils_highway_call_simulator/visualisation.py
+++ utils_highway_call_simulator/visualisation.py
@@ -20,3 +20,15 @@ def visualize_histogram(data, title, save_to="", plot=False, bins='auto'):
     if plot:
         plt.show()
     plt.close()
+
+def visualize_line(data, title, save_to="", plot=False):
+    """ Visualise data as simple line, optional 'plo' and 'save_to' args """
+    plt.plot(data)
+    plt.ylabel(title)
+    if save_to:
+        ensure_dir(save_to)
+        file_name = os.path.join(save_to, get_now_str()+"_"+title+".png")
+        plt.savefig(file_name)
+    if plot:
+        plt.show()
+    plt.close()
